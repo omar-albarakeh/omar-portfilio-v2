2c15e5980de0aac99f262aa570295e76aaef26d2
omar albarakeh <omaralbarakeh2@gmail.com>
2025-04-04 22:44:52 +0300

    adding navbar bootstrap

---
+++ src/components/navbar.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+import {
+  Navbar,
+  Container,
+  Nav,
+  NavDropdown,
+  Form,
+  FormControl,
+  Button,
+} from "react-bootstrap";
+
+function MyNavbar() {
+  return (
+    <Navbar bg="light" expand="lg">
+      <Container fluid>
+        <Navbar.Brand href="#">Navbar</Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarSupportedContent" />
+        <Navbar.Collapse id="navbarSupportedContent">
+          <Nav className="me-auto mb-2 mb-lg-0">
+            <Nav.Link href="#" className="active">
+              Home
+            </Nav.Link>
+            <Nav.Link href="#">Link</Nav.Link>
+            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#">Another action</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#">Something else here</NavDropdown.Item>
+            </NavDropdown>
+            <Nav.Link href="#" className="disabled" aria-disabled="true">
+              Disabled
+            </Nav.Link>
+          </Nav>
+          <Form className="d-flex">
+            <FormControl
+              type="search"
+              placeholder="Search"
+              className="me-2"
+              aria-label="Search"
+            />
+            <Button variant="outline-success" type="submit">
+              Search
+            </Button>
+          </Form>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default MyNavbar;
